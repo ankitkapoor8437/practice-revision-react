0e556281707cd75ddf54239a8183c155f9736c8f
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-06-06 11:44:05 +0530

    Added the reducer files

--- src/components/reducer-component/reduce.jsx
+++ src/components/reducer-component/reduce.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
 export const ReducerFunction = () => {
+  const intialState = 0;
+  const reducer = (state, action) => {
+    switch (action) {
+      case 'increment':
+        return state + 1
+
+      case 'decrement':
+        return state - 1
+
+      case 'reset':
+        return intialState
+      default:
+        return state
+    }
+  }
+
+  const [count, dispatch] = useReducer(reducer, intialState)
   return (
     <div>
-      <h1>Reducer fumction</h1>
+      <h1>Count = {count}</h1>
+      <button onClick={() => dispatch('increment')}>Increment</button>
+      <button onClick={() => dispatch('decrement')}>Decrement</button>
+      <button onClick={() => dispatch('reset')}>Reset</button>
     </div>
   )
 }
